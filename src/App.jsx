f895f7cfa4d2dcb80ddfebf0d2ba91f0015d57b4
OliGarc333 <ngarcia333@gmail.com>
2022-05-27 20:00:43 +0200

    Up to iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ import { useState } from 'react';
 function App() {
   const [contacts, setContacts] = useState(contact.slice(0, 5));
 
-  const updatedContact = () => {
+  const addRandom = () => {
     const newA = Math.floor(Math.random() * contact.length);
     const updatedContact = [...contacts, contact[newA]];
 
     setContacts(updatedContact);
   };
+  const updateByPopularity = () => {
+    setContacts([...contacts].sort((a, b) => b.popularity - a.popularity));
+  };
+  const updateByName = () => {
+    setContacts([...contacts].sort((a, b) => a.name.localeCompare(b.name)));
+  };
+
+  const removeContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick={updatedContact}>Add Random Contact</button>
+      <button onClick={addRandom}>Add Random Contact</button>
+      <button onClick={updateByPopularity}>Sort by popularity</button>
+      <button onClick={updateByName}>Sort by name</button>
+
       <table>
         <th>
           <h2>Picture</h2>
@@ -35,7 +48,7 @@ function App() {
       </table>
       {contacts.map(contact => {
         return (
-          <table>
+          <table class="GeneratedTable">
             <tbody>
               <tr>
                 <td>
@@ -43,8 +56,13 @@ function App() {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity.toFixed(2)}</td>
-                <td>{contact.wonOscar ? '🏆' : ''}</td>
-                <td>{contact.wonEmmy ? '🏆' : ''} </td>
+                <td>{contact.wonOscar && '🏆'}</td>
+                <td>{contact.wonEmmy && '🏆'} </td>
+                <td>
+                  <button onClick={() => removeContact(contact.id)}>
+                    Remove
+                  </button>
+                </td>
               </tr>
             </tbody>
           </table>
